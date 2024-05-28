be12ffc731ad155e3d467175e38596622cb9e28d
jpdelgad0 <80294133+jpdelgad0@users.noreply.github.com>
2024-05-28 10:21:11 -0400

    keras backend

--- src/wordencoder.py
+++ src/wordencoder.py
@@ -1,3 +1,12 @@
-import gensim
-model=gensim.models.Word2Vec.load_word2vec_format('./GoogleNews-vectors-negative300.bin')
+from sklearn.model_selection import train_test_split
+import numpy as np
+import keras_nlp
+import keras.backend as K
+import tensorflow as tf
+import keras
+import os
+os.environ['KERAS_BACKEND'] = 'jax'
 
+preprocessor = keras_nlp.models.BertPreprocessor.from_preset(
+    "bert_base_en"
+)
